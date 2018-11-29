13baaeffa5f8d40463976907c6a109b9bfee23b7
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-11-29 10:55:56 -0500

    100% client-rendered app, loaded immediately

---
+++ src/Placeholder.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Placeholder = () => (
+  <div className="food-image">
+    <span aria-label="placeholder" className="big-emoji" role="img">
+      🦃
+    </span>
+  </div>
+);
+
+export default Placeholder;
