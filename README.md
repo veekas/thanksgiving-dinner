5c82cc97cd9c2d973aeec7a465f582d6a46c02fd
Veekas Shrivastava <vshrivastava@ny-vshrivastavambp.intranet.theknot.com>
2018-11-29 12:23:25 -0500

    only render one course at a time (but the bundles are the same)

--- README.md
+++ README.md
@@ -8,11 +8,19 @@
 
 At this stage, we're loading images on the client-side without handling loading states. The only webpack chunks in the network tab of Chrome DevTools are the ones that come by default with create-react-app. The experience is jumpy and slow for the user.
 
-![step 1 chunks](/src/assets/step1/chunks.png)
+![step 1 chunks](src/assets/chunks-steps1-3.png)
 
 #### 2. add some loading spinners
 
 We haven't done anything to lazyload yet...
+
+#### 3. break this up into "courses" for the user
+
+The user doesn't want to see everything all at once! In this step we implemented buttons that only show one course at a time.
+
+Unfortunately, even though it appears that the components aren't loading until the user clicks the button to get their next course, the same bundles are still loading at the same time!
+
+![step 3 chunks](src/assets/chunks-steps1-3.png)
 
 ### Thanks
 
--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ import Turkey from './foods/Turkey';
 import './App.css';
 
 class App extends Component {
+  state = {
+    showCourse1: true,
+    showCourse2: false,
+    showCourse3: false,
+  }
+
+  handleClick = (course) => {
+    const base = {
+      showCourse1: false,
+      showCourse2: false,
+      showCourse3: false,
+    }
+
+    const courseKey = `showCourse${course}`;
+
+    this.setState({ ...base, [courseKey]: true });
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,11 +34,23 @@ class App extends Component {
         <header className="header">Thanksgiving Dinner</header>
 
         <div className="plate-contents">
-          <Turkey />
-          <Stuffing />
-          <MashedPotatoes />
-          <Gravy />
-          <Pie />
+          {this.state.showCourse1 ? <Turkey /> : null }
+          {this.state.showCourse1 ? <Stuffing /> : null }
+          {this.state.showCourse2 ? <MashedPotatoes /> : null }
+          {this.state.showCourse2 ? <Gravy /> : null }
+          {this.state.showCourse3 ? <Pie /> : null }
+        </div>
+
+        <div className="buttons">
+          <button onClick={() => this.handleClick(1)}>
+            Main Course
+          </button>
+          <button onClick={() => this.handleClick(2)}>
+            Sides
+          </button>
+          <button onClick={() => this.handleClick(3)}>
+            Dessert
+          </button>
         </div>
 
         <div className="plate" />
